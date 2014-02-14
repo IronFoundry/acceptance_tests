a00df5ba0187f86b152f4c8d34e801099b8e5838
Eric Lee <saintgimp@hotmail.com>
2014-02-14 14:48:31 -0800

    Support both Windows and Linux, and other changes

--- push_spec.rb
+++ push_spec.rb
@@ -1,11 +1,14 @@
 require 'rspec'
 require 'open-uri'
+require 'socket'
 
 module IronFoundry
   describe "when targeting ironfoundry" do
 
     def runcf(cmd)
       expanded_cmd = "cf --script #{cmd} 2>&1"
+      expanded_cmd.gsub('/', '\\') if RUBY_PLATFORM !~ /linux/
+
       output = `#{expanded_cmd}`
       output = output.gsub(/Fast Debug.+\n/, '')
       return output
@@ -14,9 +17,9 @@ module IronFoundry
     before :all do
       @domain= 'qa.ironfoundry.org'
       @endpoint = 'http://api.' + @domain
-      @appname =  'ATApp01'
+      @appname = Socket.gethostname + '-acceptance'
       @user = 'admin'
-      @password = '<yourpasswordhere>'
+      @password = 'put-password-here'
       @test_org =  'Tier3'
       @test_space = 'T3QA'
 
@@ -26,13 +29,13 @@ module IronFoundry
       result = runcf("login --username #{@user} --password #{@password} --organization #{@test_org}  --space #{@test_space}")
       expect(result).to be_empty
 
-      result = runcf('delete -all -routes')
-      # expect(result).to match(/Unknown app/i).or eq('')
+      result = runcf("delete #{@appname} --routes")
+      expect(result).to match(/Unknown app/i) if !result.empty?
     end
 
     context 'when .net app is pushed' do
         before(:all) do
-        @result = runcf("push #{@appname} --path assets\\asp_net_app --stack mswin-clr")
+        @result = runcf("push #{@appname} --path assets/asp_net_app --stack mswin-clr")
       end
 
       it 'reports success after push' do
@@ -48,6 +51,9 @@ module IronFoundry
       it 'reports application stats'
       it 'returns latest logs'
       it 'can be deleted'
+      it 'can be pushed twice'
+      it 'can set environment variables'
+      it 'can push a node app (windows dea/warden can co-exist with linus dea/warden)'
 
     end
   end
