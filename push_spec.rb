6adcfd8d733daeebdc5eb7f366ec8d82d15e266a
Eric Lee <saintgimp@hotmail.com>
2014-02-14 16:07:04 -0800

    Test stopping an app

--- push_spec.rb
+++ push_spec.rb
@@ -3,15 +3,26 @@ require 'open-uri'
 require 'socket'
 
 module IronFoundry
-  describe "when targeting ironfoundry" do
+  describe 'when targeting ironfoundry' do
 
-    def runcf(cmd)
+    def execute(cmd)
       expanded_cmd = "cf --script #{cmd} 2>&1"
       expanded_cmd.gsub('/', '\\') if RUBY_PLATFORM !~ /linux/
 
       output = `#{expanded_cmd}`
       output = output.gsub(/Fast Debug.+\n/, '')
       return output
+    end
+
+    def ensure_app_is_deleted
+      result = execute("delete #{@appname} --routes")
+      expect(result).to match(/Unknown app/i) unless result.empty?
+    end
+
+    def ensure_app_is_pushed
+      ensure_app_is_deleted
+      result = execute("push #{@appname} #{@app_options}")
+      expect(result).to match(/Push successful!/i)
     end
 
     before :all do
@@ -25,19 +36,18 @@ module IronFoundry
       @test_org =  'Tier3'
       @test_space = 'T3QA'
 
-      result = runcf("target #{@endpoint}")
+      result = execute("target #{@endpoint}")
       expect(result).to be_empty
 
-      result = runcf("login --username #{@user} --password #{@password} --organization #{@test_org}  --space #{@test_space}")
+      result = execute("login --username #{@user} --password #{@password} --organization #{@test_org}  --space #{@test_space}")
       expect(result).to be_empty
     end
 
     context 'when .net app is pushed' do
       before(:all) do
-        result = runcf("delete #{@appname} --routes")
-        expect(result).to match(/Unknown app/i) if !result.empty?
+        ensure_app_is_deleted
 
-        @push_result = runcf("push #{@appname} #{@app_options}")
+        @push_result = execute("push #{@appname} #{@app_options}")
       end
 
       it 'reports success after push' do
@@ -50,11 +60,15 @@ module IronFoundry
       end
 
       it 'is in the list of apps' do
-        result = runcf("apps")
+        result = execute('apps')
         expect(result).to match(/#{@appname}/i)
       end
 
-      it 'health reports running'
+      it 'health reports as running' do
+        result = execute("health #{@appname}")
+        expect(result).to match(/running/i)
+      end
+
       it 'reports application stats'
       it 'returns latest logs'
       it 'can be pushed twice'
@@ -65,14 +79,13 @@ module IronFoundry
 
     context 'when .net app is deleted' do
       before(:all) do
-        result = runcf("push #{@appname} #{@app_options}")
-        expect(result).to match(/Push successful!/i)
+        ensure_app_is_pushed
 
-        @delete_result = runcf("delete #{@appname} --routes")
+        @delete_result = execute("delete #{@appname} --routes")
       end
 
       it 'reports success after delete' do
-        expect(@delete_result).to eq('')
+        expect(@delete_result).to be_empty
       end
 
       it 'is not addressable at expected endpoint' do
@@ -80,8 +93,30 @@ module IronFoundry
       end
 
       it 'is not in the list of apps' do
-        result = runcf("apps")
+        result = execute('apps')
         expect(result).to_not match(/#{@appname}/i)
+      end
+
+    end
+
+    context 'when running .net app is stopped' do
+      before(:all) do
+        ensure_app_is_pushed
+
+        @stop_result = execute("stop #{@appname}")
+      end
+
+      it 'reports success' do
+        expect(@stop_result).to be_empty
+      end
+
+      it 'is not addressable at expected endpoint' do
+        expect { open("http://#{@appname}.#{@domain}") }.to raise_error(/404/)
+      end
+
+      it 'health reports as stopped' do
+        result = execute("health #{@appname}")
+        expect(result).to match(/stopped/i)
       end
 
     end
