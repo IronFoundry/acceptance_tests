6542e841ecbd25dbf9e6e7029e596421a4286bcb
Bob Brumfield <bob.brumfield@gmail.com>
2014-03-10 10:40:47 -0700

    Added prime sieve to drive cpu usage for monitoring tests

---
+++ test_apps/asp_net_app/asp_net_app/Controllers/PrimeController.cs
@@ -0,0 +1,30 @@
+﻿using asp_net_app.Prime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asp_net_app.Controllers
+{
+    public class PrimeController : Controller
+    {
+        //
+        // GET: /Prime/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PrimeSearch(int PrimeSearchValue)
+        {
+            var sieve = new PrimeSieve(PrimeSearchValue);
+            sieve.ComputePrimes();
+            var values = sieve.ToArray();
+
+            ViewBag.PrimeNumbers = values;
+
+            return View("Index");
+        }
+	}
+}
