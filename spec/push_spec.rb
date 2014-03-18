311e7e23e468ac6fb61273f18d7b23dc37143f08
Eric Lee <saintgimp@hotmail.com>
2014-03-18 14:38:17 -0700

    Add test for pushing twice in a row

--- spec/push_spec.rb
+++ spec/push_spec.rb
@@ -33,10 +33,35 @@ describe 'when .net app is pushed' do
   end
 
   it 'reports application stats'
-  it 'returns latest logs'
-  it 'can be pushed twice'
-  it 'can set environment variables'
-  it 'can push a node app (windows dea/warden can co-exist with linux dea/warden)'
   it 'should push multiple instances'
 end
 
+describe 'when .net app is pushed twice in a row' do
+  include_context 'when targeting ironfoundry'
+
+  before(:all) do
+    ensure_clean_app_is_pushed
+
+    @push_result = execute("push #{@appname} #{@app_options}")
+  end
+
+  it 'reports success after push' do
+    expect(@push_result).to match(/#0\s+running/i)
+  end
+
+  it 'is addressable at expected endpoint' do
+    response = open(app_endpoint)
+    expect(response.status).to include('200')
+  end
+
+  it 'is in the list of apps' do
+    result = execute('apps')
+    expect(result).to match(/#{@appname}/i)
+  end
+
+  it 'app status reports as running' do
+    result = execute("app #{@appname}")
+    expect(result).to match(/requested state: started/i)
+    expect(result).to match(/#0\s+running/i)
+  end
+end
